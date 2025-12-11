b47edd130ca78675f5a2f40207ed3940e262693f
LGtkz <leozingabr@gmail.com>
2025-12-11 10:14:13 -0300

    teste

--- src/app/ordem-servico/page.js
+++
@@ -1,5 +0,0 @@
-export default function OrdemServico(){
-    return(
-        <h1>Ordem de Serviço</h1>
-    )
-}
--- src/components/relatorioHome/relatorioHome.js
+++
@@ -1,7 +0,0 @@
-export default function Relatorio() {
-    return (
-        <div>
-            <h2>Relatório Home</h2>
-        </div>
-    )
-}       
